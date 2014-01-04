f304ad24f2e5c0b3b8a71edef87226b37c520f68
HeartOut <maxio808@163.com>
2014-01-04 19:26:51 +0800

    tan

--- tanbirthday/js/data.js
+++ tanbirthday/js/data.js
@@ -2,18 +2,34 @@
 
 var jsonData=[
 {
-	cont:"加油！",
-	name:"guest"
+	cont:"湉湉，生日快乐！Happy Birthday！",
+	name:"HeartOut"
 	
 },
 {
-	cont:"加油！！",
-	name:"guest"
+	cont:"湉湉，加油！！",
+	name:"HeartOut"
 	
 },
 {
-	cont:"加油！！！",
-	name:"guest"
+	cont:"于湉，21岁生日快乐",
+	name:"HeartOut"
 	
 },
+{
+	cont:"湉湉，要听你唱10年，20年，100年！！",
+	name:"湉湉圈"
+	
+},
+{
+	cont:"于湉，生日吃得胖点！！！",
+	name:"湉湉圈"
+	
+},
+{
+	cont:"我爱于湉！！祝1月10日天津歌友会成功！",
+	name:"湉湉圈"
+	
+},
+
 ];

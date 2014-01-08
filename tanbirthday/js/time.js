e5cacd00659090ec9e679e7037d6bc7bd05d01ff
HeartOut <maxio808@163.com>
2014-01-08 21:46:11 +0800

    tan

--- tanbirthday/js/time.js
+++ tanbirthday/js/time.js
@@ -103,7 +103,7 @@ $(function(){
 		{
 		$(".container").hide();
 		$(".title").hide();
-		$(".all").append("<div class='happy'>于湉生日快乐</div>");
+		$(".all").append("<div class='happy'><img src='images/birthday.png'></img></div>");
 		}
 	}
 	
@@ -113,36 +113,3 @@ $(function(){
 
 })
 
-var jsonData=[
-{
-	cont:"湉湉，生日快乐！Happy Birthday！",
-	name:"HeartOut"
-	
-},
-{
-	cont:"湉湉，加油！！",
-	name:"HeartOut"
-	
-},
-{
-	cont:"于湉，21岁生日快乐",
-	name:"HeartOut"
-	
-},
-{
-	cont:"湉湉，要听你唱10年，20年，100年！！",
-	name:"湉湉圈"
-	
-},
-{
-	cont:"于湉，生日吃得胖点！！！",
-	name:"湉湉圈"
-	
-},
-{
-	cont:"我爱于湉！！祝1月10日天津歌友会成功！",
-	name:"湉湉圈"
-	
-},
-
-];
